ac93041d5e5b480262b0fd54c15ec72b859aff88
nones5 <amir.makmur32@gmail.com>
2020-03-22 10:14:36 +0800

    update scripts

--- script.js
+++ script.js
@@ -8,20 +8,34 @@ xhr.onreadystatechange = function () {
             let results = data["attributes"];
             let colors = arr[Math.floor(Math.random() * arr.length)];
 
-            $('#data-covid').append(`<div class="col-row">
-            <div class="card text-white text-center shadow `+colors+`  mt-5 mb-3"">
-                <div class="card-header font-weight-bold">`+results["Country_Region"]+`</div>
-                <div class="card-body">
-                    <p class="card-text font-weight-bold">Confirmed  : `+results["Confirmed"]+`</p>
-                    <p class="card-text font-weight-bold">Deaths     : `+results["Deaths"]+`</p>
-                    <p class="card-text font-weight-bold">Recovered  : `+results["Recovered"]+`</p>
+            $('#data-covid').append(`
+            <div class="col-md-4">
+                <div class="card text-white text-center shadow `+colors+`  mt-5 mb-3"">
+                    <div class="card-header font-weight-bold">`+results["Country_Region"]+`</div>
+                    <div class="card-body text-left">
+                        <p class="card-text font-weight-bold">🤕Confirmed  : `+addCommas(results["Confirmed"])+`</p>
+                        <p class="card-text font-weight-bold">☠Deaths     : `+addCommas(results["Deaths"])+`</p>
+                        <p class="card-text font-weight-bold">😘Recovered  : `+addCommas(results["Recovered"])+`</p>
+                    </div>
                 </div>
-            </div>
-        </div>`);
-        i++
+            </div>`);
         });
     }
 }
+        
 xhr.open('GET', 'https://api.kawalcorona.com/', true);
 xhr.send();
+
+
+function addCommas(nStr){
+    nStr += '';
+    var x = nStr.split('.');
+    var x1 = x[0];
+    var x2 = x.length > 1 ? '.' + x[1] : '';
+    var rgx = /(\d+)(\d{3})/;
+    while (rgx.test(x1)) {
+        x1 = x1.replace(rgx, '$1' + '.' + '$2');
+    }
+    return x1 + x2;
+}
 
